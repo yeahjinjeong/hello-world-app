8f10f1ecd0a79535ef58c71367e5150517ec7163
yeahjinjeong <yeahjin311@sookmyung.ac.kr>
2024-07-02 10:24:50 +0900

    Feat: hello-world-app init

---
+++ src/main/java/com/sh/app/AppController.java
@@ -0,0 +1,22 @@
+package com.sh.app;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AppController {
+
+    @Value("${app.version}")
+    private String appVersion;
+
+    @GetMapping("/")
+    @ResponseBody
+    public String index() {
+        return """
+            <h1>🌞Welcome to Hello World Application🌞</h1>
+            <h4>version %s</h4>
+            """.formatted(appVersion);
+    }
+}
